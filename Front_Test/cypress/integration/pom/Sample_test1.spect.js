39dd15e754869b43944de00fff2b79097130c856
Jose Sosa <testrepo.autom@gmail.com>
2022-04-26 19:49:34 -0500

    fist commit

---
+++ Front_Test/cypress/integration/pom/Sample_test1.spect.js
@@ -0,0 +1,21 @@
+describe('My first test', () => {
+  beforeEach(() => {
+    // Cypress starts out with a blank slate for each test
+    // so we must tell it to visit our website with the `cy.visit()` command.
+    // Since we want to visit the same URL at the start of all our tests,
+    // we include it in our beforeEach function so that it runs before each test
+    cy.visit('/')
+  })
+   it('can add new todo items', () => {
+    // We'll store our item text in a variable so we can reuse it
+    const newItem = 'Alexa'
+
+    // Let's get the input element and use the `type` command to
+    // input our new list item. After typing the content of our item,
+    // we need to type the enter key as well in order to submit the input.
+    // This input has a data-test attribute so we'll use that to select the
+    // element in accordance with best practices:
+    // https://on.cypress.io/selecting-elements
+    cy.get('[id=twotabsearchtextbox]').type(`${newItem}{enter}`)
+  })
+})
